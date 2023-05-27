a29e429a61a3a9ff9f801679526cdc91bb92ea6e
prayatnaaa <tudeprayatna04@gmail.com>
2023-05-27 16:12:18 +0800

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,28 +11,32 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 4040), 0);
+//        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 4040), 0);
+//
+//        httpServer.createContext("/melbourne", new MelbourneHandler());
+//        httpServer.setExecutor(Executors.newSingleThreadExecutor());
+//        httpServer.start();
 
-        httpServer.createContext("/melbourne", new MelbourneHandler());
-        httpServer.setExecutor(Executors.newSingleThreadExecutor());
-        httpServer.start();
+ Connect.connect();
+
+
     }
 
-    public static class MelbourneHandler implements HttpHandler {
-
-        @Override
-        public void handle(HttpExchange exchange) throws IOException{
-
-            if("GET".equals(exchange.getRequestMethod())){
-
-                OutputStream outputStream = exchange.getResponseBody();
-                String ResponseToSendBack = "Hello from Melbourne";
-                exchange.sendResponseHeaders(200, ResponseToSendBack.length());
-
-                outputStream.write(ResponseToSendBack.getBytes());
-                outputStream.flush();
-                outputStream.close();
-            }
-        }
-    }
+//    public static class MelbourneHandler implements HttpHandler {
+//
+//        @Override
+//        public void handle(HttpExchange exchange) throws IOException{
+//
+//            if("GET".equals(exchange.getRequestMethod())){
+//
+//                OutputStream outputStream = exchange.getResponseBody();
+//                String ResponseToSendBack = "Hello from Melbourne";
+//                exchange.sendResponseHeaders(200, ResponseToSendBack.length());
+//
+//                outputStream.write(ResponseToSendBack.getBytes());
+//                outputStream.flush();
+//                outputStream.close();
+//            }
+//        }
+//    }
 }

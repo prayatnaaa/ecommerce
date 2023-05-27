8635c4459fc683215da9bddbc6e4b239be790b22
prayatnaaa <tudeprayatna04@gmail.com>
2023-05-27 23:06:48 +0800

    create connect.java class to get data from database

--- src/main/java/org/example/Connect.java
+++ src/main/java/org/example/Connect.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Connect {
     public static void connect() {
@@ -10,7 +8,7 @@ public class Connect {
         Connection conn = null;
         try {
 
-            String url = "jdbc:sqlite:" + rootPath + "/db_ecommerce";
+            String url = "jdbc:sqlite:" + rootPath + "/db_ecommerce.db";
             conn = DriverManager.getConnection(url);
 
             System.out.println("Connection to SQLite has been established.");
@@ -25,6 +23,37 @@ public class Connect {
             } catch (SQLException ex) {
                 System.out.println(ex.getMessage());
             }
+        }
+    }
+
+    private Connection connection() {
+
+        String rootPath = System.getProperty("user.dir");
+        String url = "jdbc:sqlite:" + rootPath + "/db_ecommerce.db";
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(url);
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void selectAll() throws SQLException{
+        String sql = "SELECT * FROM users";
+
+        try (Connection conn = connection();
+             Statement statement  = conn.createStatement();
+             ResultSet rs    = statement.executeQuery(sql)){
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" +
+                        rs.getString("last_name") + "\t" +
+                        rs.getString("type"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 }

d61bf83c23dddffca9c371f7a0dcb2c53ae7f0e0
prayatnaaa <tudeprayatna04@gmail.com>
2023-05-29 00:06:28 +0800

    update classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,42 +1,16 @@
 package org.example;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-//        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 4040), 0);
-//
-//        httpServer.createContext("/melbourne", new MelbourneHandler());
-//        httpServer.setExecutor(Executors.newSingleThreadExecutor());
-//        httpServer.start();
+    public static void main(String[] args) throws Exception {
 
- Connect.connect();
-
-
+//        ConvertData convertData=new ConvertData();
+//        Connect.connect();
+//        Connect con= new Connect();
+//        con.selectAll();
+        serverConnection.startServer();
+//        convertData.DataToJSON();
     }
-
-//    public static class MelbourneHandler implements HttpHandler {
-//
-//        @Override
-//        public void handle(HttpExchange exchange) throws IOException{
-//
-//            if("GET".equals(exchange.getRequestMethod())){
-//
-//                OutputStream outputStream = exchange.getResponseBody();
-//                String ResponseToSendBack = "Hello from Melbourne";
-//                exchange.sendResponseHeaders(200, ResponseToSendBack.length());
-//
-//                outputStream.write(ResponseToSendBack.getBytes());
-//                outputStream.flush();
-//                outputStream.close();
-//            }
-//        }
-//    }
 }

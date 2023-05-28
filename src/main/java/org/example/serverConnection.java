5dd43b652a7c486dd18b967a7165657deb9f7ea1
prayatnaaa <tudeprayatna04@gmail.com>
2023-05-28 23:57:48 +0800

    add PUT, POST, DELETE, and GET server Connection!

--- src/main/java/org/example/serverConnection.java
+++ src/main/java/org/example/serverConnection.java
@@ -3,8 +3,10 @@ package org.example;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
@@ -26,10 +28,11 @@ private static final int p=8068;
             public void handle(HttpExchange exchange) throws IOException {
                 Query connect = new Query();
 
-                String ResponseToSendBack = null;
+                String ResponseToSendBack = "";
                 OutputStream outputStream = exchange.getResponseBody();
                 String method = exchange.getRequestMethod();
                 String path = exchange.getRequestURI().getPath();
+
 
                 if ("GET".equals(method) && "/users".equals(path)) {
                     outputStream = exchange.getResponseBody();
@@ -39,7 +42,7 @@ private static final int p=8068;
                         throw new RuntimeException(e);
                     }
 
-                } else if ("GET".equals(method) && path.matches("/users/\\d+")) {
+                } else if ("GET".equals(method) && path.matches("/user/\\d+")) {
                     String[] pathParts = path.split("/");
                     int userId = Integer.parseInt(pathParts[pathParts.length - 1]);
 
@@ -50,7 +53,7 @@ private static final int p=8068;
                         throw new RuntimeException(e);
                     }
 
-                } else if("DELETE".equals(method) && path.matches("/users/\\d+")){
+                } else if("DELETE".equals(method) && path.matches("/user/\\d+")){
                     String[] pathParts = path.split("/");
                     int userId = Integer.parseInt(pathParts[pathParts.length - 1]);
 
@@ -63,6 +66,13 @@ private static final int p=8068;
                     } catch (Exception e) {
                         throw new RuntimeException(e);
                     }
+                } else if("POST".equals(method) && path.matches("/user")){
+                    JSONObject requestBodyJSON = parseRequestBody(exchange.getRequestBody());
+                    ResponseToSendBack = connect.postMethod(requestBodyJSON);
+                } else if ("PUT".equals(method) && path.matches("/user/\\d+")){
+                    JSONObject requestBodyJSON = parseRequestBody(exchange.getRequestBody());
+                    ResponseToSendBack = connect.putMethod(path, requestBodyJSON);
+
                 }
                 exchange.getResponseHeaders().set("Content-Type", "application/json");
                 exchange.sendResponseHeaders(200, ResponseToSendBack.length());
@@ -70,5 +80,13 @@ private static final int p=8068;
                 outputStream.flush();
                 outputStream.close();
             }
+
+        }
+
+        private static JSONObject parseRequestBody(InputStream reqBody) throws IOException{
+
+            byte[] requestBodyBytes = reqBody.readAllBytes();
+            String reqBodyString = new String(requestBodyBytes);
+            return new JSONObject(reqBodyString);
         }
     }

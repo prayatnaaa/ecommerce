3578c371b87476fbfc8a468be64bcf0352ba0a52
prayatnaaa <tudeprayatna04@gmail.com>
2023-05-30 20:26:07 +0800

    deleting comments in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-//        ConvertData convertData=new ConvertData();
-//        Connect.connect();
-//        Connect con= new Connect();
-//        con.selectAll();
         serverConnection.startServer();
-//        convertData.DataToJSON();
     }
 }
